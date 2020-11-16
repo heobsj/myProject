8d5d054724be8134fb329226f0db53ba79b4a6ba
heobsj <heo@ceedup.com>
2020-11-16 17:00:49 +0900

    01

---
+++ caculator01/caculator01/소스.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "Çì´õ.h"
+int main() {
+	script_First();
+	script_Last();
+
+}
---
+++ caculator01/caculator01/헤더.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <stdio.h>
+
+void script_First() {
+	printf("사칙연산 시작\n");
+}
+void script_Last() {
+	printf("사칙연산 끝\n");
+}
